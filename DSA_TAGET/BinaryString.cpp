6ecf0d252b40dffccc036993016e4705e13d2495
NTLONG <thanhlong0224@gmail.com>
2022-05-24 20:19:34 +0700

    ==

--- DSA_TAGET/BinaryString.cpp
+++ DSA_TAGET/BinaryString.cpp
@@ -1,54 +1,20 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
-
-struct node
-{
-	int data;
-	struct node* next;
-};
-typedef struct node* Node;
-
-void insertNodePoision(Node &head, int data, int vt)
-{
-	Node tmp = new node;
-	tmp->data = data;
-	tmp->next = nullptr;
-
-	Node prv = nullptr, cur = head;
-	if(!head || vt == 0)
-	{
-		tmp->next = head;
-		head = tmp;
-		return;
-	}
-
-	for(int i=0; i<vt && cur != nullptr; i++)
-	{
-		prv = cur;
-		cur = cur->next;
-	}
-
-	prv->next = tmp;
-	tmp->next = cur;
-}
-
-void printList(Node head)
-{
-	while(head != nullptr)
-	{
-		cout << head->data << " ";
-		head = head->next;
-	}
-}
 
 int main()
 {
-	Node head = nullptr;
+	ifstream f;
+	f.open("HiDay.txt", ios::in);
+	f.seekg(4, ios_base::cur);
+	f.seekg(-1, ios_base::cur);
 
-	for(int i=0; i<=10; i++)
-		insertNodePoision(head, i, i);
-	insertNodePoision(head, 2002, 8);
+	string s;
+	getline(f, s);
+	cout << s << endl;
 
-	printList(head);
+	f.close();
+
 	return 0;
 }
