01b1515bfeb182705dc26d35bb86f613fcaaa182
NTLONG <thanhlong0224@gmail.com>
2022-06-13 11:58:14 +0700

    13/6/2022

--- DSA_TAGET/TestHello.cpp
+++ DSA_TAGET/TestHello.cpp
@@ -808,11 +808,518 @@ Node reverseGroup(Node head, const int k)
 	return prv;
 }
 
+Node reverseGroupByStack(Node head, int k)
+{
+	int flag = 1;
+	Node newHead = nullptr, track = nullptr, cur = head;
+	while(cur != nullptr)
+	{
+		int count = 0;
+		if(flag)
+		{
+			stack<Node> st;
+
+			while(cur != nullptr && count < k)
+			{
+				st.push(cur);
+				cur = cur->next;
+				st.top()->next = nullptr;
+				count++;
+			}
+
+			while(!st.empty())
+			{
+				if(newHead == nullptr)
+				{
+					newHead = st.top();
+					track = st.top();
+					st.pop();
+				}
+				else
+				{
+					track->next = st.top();
+					st.pop();
+					track = track->next;
+				}
+			}
+			flag = 0;
+		}
+		else
+		{
+			while(cur != nullptr && count < k)
+			{
+				track->next = cur;
+				track = track->next;
+				cur = cur->next;
+				count++;
+			}
+			flag = 1;
+		}
+	}
+
+	return newHead;
+}
+
+Node alternateOddEven(Node head)
+{
+	// if(head == nullptr || head->next == nullptr)
+	// 	return head;
+
+	// int flag = !(head->data & 1);
+	// Node updateLink = head, cur = head->next, prv = head;
+
+	// while(cur)
+	// {
+	// 	if(flag)
+	// 	{
+	// 		while(cur != nullptr && !(cur->data & 1))
+	// 		{
+	// 			prv = cur;
+	// 			cur = cur->next;
+	// 		}
+
+	// 		if(cur != nullptr)
+	// 		{
+	// 			prv->next = cur->next;
+	// 			cur->next = updateLink->next;
+	// 			updateLink->next = cur;
+	// 			updateLink = updateLink->next;
+	// 			cur = updateLink->next;
+	// 			prv = updateLink;
+	// 		}
+
+	// 		flag = 0;
+	// 	}
+	// 	else
+	// 	{
+	// 		while(cur != nullptr && (cur->data & 1))
+	// 		{
+	// 			prv = cur;
+	// 			cur = cur->next;
+	// 		}
+
+	// 		if(cur != nullptr)
+	// 		{
+	// 			prv->next = cur->next;
+	// 			cur->next = updateLink->next;
+	// 			updateLink->next = cur;
+	// 			updateLink = updateLink->next;
+	// 			cur = updateLink->next;
+	// 			prv = updateLink;
+	// 		}
+			
+	// 		flag = 1;
+	// 	}
+	// }
+
+	// return head;
+
+	int flag = !(head->data & 1);
+	Node odd, even, headOdd, headEven, track;
+	even = odd = headOdd = headEven = nullptr;
+	track = head;
+
+	for(Node k = head->next; k != nullptr; k = k->next)
+	{
+		if(k->data & 1)
+		{
+			if(odd == nullptr)
+				headOdd = odd = k;
+			else
+			{
+				odd->next = k;
+				odd = odd->next;
+			}
+		}
+		else
+		{
+			if(even == nullptr)
+				headEven = even = k;
+			else
+			{
+				even->next = k;
+				even = even->next;
+			}
+		}
+	}
+
+	if(headOdd != nullptr)
+		odd->next = nullptr;
+	else if(headEven != nullptr)
+		even->next = nullptr;
+
+	if(flag)
+	{
+		while(headOdd && headEven)
+		{
+			track->next = headOdd;
+			headOdd = headOdd->next;
+			track->next->next = headEven;
+			track = headEven;
+			headEven = headEven->next;
+		}
+	}
+	else
+	{
+		while(headOdd && headEven)
+		{
+			track->next = headEven;
+			headEven = headEven->next;
+			track->next->next = headOdd;
+			track = headOdd;
+			headOdd = headOdd->next;
+		}
+	}
+
+	if(headOdd)
+		track->next = headOdd;
+	else if(headEven)
+		track->next = headEven;
+
+	return head;
+}
+
+Node alternateOddEvenByStack(Node head)
+{
+	int flag = !(head->data & 1);
+	stack<Node> odd, even;
+	Node track = nullptr;
+
+	for(Node k = head->next; k != nullptr; k = k->next)
+	{
+		if(k->data & 1)
+			odd.push(k);
+		else
+			even.push(k);
+	}
+
+	if(odd.size() > even.size())
+	{
+		while(odd.size() > even.size())
+		{
+			odd.top()->next = track;
+			track = odd.top();
+			odd.pop();
+		}
+	}
+	else if(odd.size() < even.size())
+	{
+		while(odd.size() < even.size())
+		{
+			even.top()->next = track;
+			track = even.top();
+			even.pop();
+		}
+	}
+
+	if(flag)
+	{
+		while(odd.size())
+		{
+			even.top()->next = track;
+			odd.top()->next = even.top();
+			track = odd.top();
+			odd.pop();
+			even.pop();
+		}
+	}
+	else
+	{
+		while(even.size())
+		{
+			odd.top()->next = track;
+			even.top()->next = odd.top();
+			track = even.top();
+			even.pop();
+			odd.pop();
+		}
+	}
+
+	head->next = track;
+
+	return head;
+}
+
+Node alternateOddEvenDiffPos(Node head)
+{
+	// int i = 1;
+	// stack<Node> odd, even;
+
+	// for(Node k = head; k != nullptr; k = k->next)
+	// {
+	// 	if((i & 1) && !(k->data & 1))
+	// 		odd.push(k);
+	// 	else if(!(i & 1) && (k->data & 1))
+	// 		even.push(k);
+	// 	i++;
+	// }
+
+	// while(odd.size() && even.size())
+	// {
+	// 	swap(odd.top()->data, even.top()->data);
+	// 	odd.pop();
+	// 	even.pop();
+	// }
+
+	// return head;
+
+	Node dummyOdd, dummyEven, odd, even;
+	dummyOdd = new node;
+	dummyEven = new node;
+	odd = dummyOdd;
+	even = dummyEven;
+
+	for(Node k = head; k != nullptr; k = k->next)
+	{
+		if(k->data & 1)
+		{
+			odd->next = k;
+			odd = odd->next; 
+		}
+		else
+		{
+			even->next = k;
+			even = even->next;
+		}
+	}
+
+	odd->next = even->next = nullptr;
+	odd = dummyOdd->next;
+	even = dummyEven->next;
+	delete dummyOdd;
+	delete dummyEven;
+
+	if(odd)
+	{
+		head = odd;
+		Node track = head;
+		odd = odd->next;
+
+		while(odd != nullptr && even != nullptr)
+		{
+			track->next = even;
+			even = even->next;
+			track->next->next = odd;
+			track = odd;
+			odd = odd->next;
+		}
+
+		if(odd)
+			track->next = odd;
+		else if(even)
+			track->next = even;
+	}
+	else
+		return even;
+
+	return head;
+}
+
+Node deleteNodeAlternate(Node head)
+{
+	// Node cur = head;
+	// while(cur != nullptr && cur->next != nullptr)
+	// {
+	// 	Node dele = cur->next;
+	// 	cur->next = cur->next->next;
+	// 	cur = cur->next;
+	// 	delete dele;
+	// }
+
+	// return head;
+
+	if(head == nullptr || head->next == nullptr)
+		return head;
+
+	Node dele = head->next;
+	head->next = dele->next;
+	delete dele;
+	deleteNodeAlternate(head->next);
+	
+	return head;
+}
+
+bool identicalLL(Node head1, Node head2)
+{
+	while(head1 != nullptr && head2 != nullptr)
+	{
+		if(head1->data != head2->data)
+			return false;
+		head1 = head1->next;
+		head2 = head2->next;
+	}
+
+	if(head1 != nullptr || head2 != nullptr)
+		return false;
+	return true;
+}
+
+bool identicalLLRecur(Node head1, Node head2)
+{
+	if(head1 == nullptr && head2 == nullptr)
+		return true;
+	if(head1 == nullptr || head2 == nullptr || (head1->data != head2->data))
+		return false;
+	return identicalLLRecur(head1->next, head2->next);
+}
+
+Node deleteNodesValGreaterRight(Node head)
+{
+	if(head == nullptr || head->next == nullptr)
+		return head;
+
+	Node reverNode = reverLL(head);
+	Node cur = reverNode->next, prv = reverNode;
+	int maxN = reverNode->data;
+
+	while(cur != nullptr)
+	{
+		if(cur->data <= maxN)
+		{
+			Node x = cur->next;
+			prv->next = x;
+			delete cur;
+			cur = x;
+		}
+		else
+		{
+			prv = cur;
+			maxN = cur->data;
+			cur = cur->next;
+		}
+	}
+
+	return reverLL(reverNode);
+}
+
+Node addTwoNumLL(Node head1, Node head2)
+{
+	int check = 0;
+	Node add = new node;
+	Node result = add;
+
+	head1 = reverLL(head1);
+	head2 = reverLL(head2);
+
+	Node rec1 = head1, rec2 = head2;
+
+	while(head1 != nullptr && head2 != nullptr)
+	{
+		Node element = new node;
+		element->data = (head1->data + head2->data + check) % 10;
+
+		check = (head1->data + head2->data + check) / 10;
+
+		add->next = element;
+		add = add->next;
+		head1 = head1->next;
+		head2 = head2->next;
+	}
+
+	while(head1 != nullptr)
+	{
+		Node element = new node;
+		element->data = (head1->data + check) % 10;
+
+		check = (head1->data + check) / 10;
+
+		add->next = element;
+		add = add->next;
+		head1 = head1->next;
+	}
+
+	while(head2 != nullptr)
+	{
+		Node element = new node;
+		element->data = (head2->data) % 10;
+
+		check = (head2->data + check) / 10;
+
+		add->next = element;
+		add = add->next;
+		head2 = head2->next;
+	}
+
+	if(check)
+	{
+		Node element = new node;
+		element->data = 1;
+		add->next = element;
+		add = add->next;
+	}
+	add->next = nullptr;
+
+	add = result;
+	result = reverLL(result->next);
+	delete add;
+
+	reverLL(rec1);
+	reverLL(rec2);
+
+	return result;
+}
+
+void deleteLLConstrait(Node head, Node dele)
+{
+	if(dele == head)
+	{
+		if(head->next == nullptr)
+		{
+			cout << "No dele" << endl;
+			return;
+		}
+		else
+		{
+			head->data = head->next->data;
+			dele = head->next;
+			head->next = head->next->next;
+		}
+	}
+	else
+		while(head->next != nullptr && head->next != dele)
+			head = head->next;
+
+	if(head->next == nullptr)
+	{
+		cout << "No dele" << endl;
+		return;
+	}
+	else
+	{
+		head->next = dele->next;
+		delete dele;
+	}
+}
+
+Node rotateLLByK(Node head, int k)
+{
+	Node newHead = head, run = head;
+
+	k = k % countLengthLL(head);
+
+	if(k != 0)
+	{
+		for(int i=1; i<k; i++)
+			run = run->next;
+
+		newHead = run->next;
+		run->next = nullptr;
+		run = newHead;
+
+		while(run->next != nullptr)
+			run = run->next;
+
+		run->next = head;
+	}
+
+	return newHead;
+}
+
 int main()
 {
-	Node head1 = nullptr;
-	int n, k, x;
-	cin >> n >> k;
+	Node head1 = nullptr, head2 = nullptr;
+	int n, m, x;
+	cin >> n >> m;
 
 	for(int i=0; i<n; i++)
 	{
@@ -820,9 +1327,11 @@ int main()
 		insertNodePosition(head1, x, i);
 	}
 
-	head1 = reverseGroup(head1, k);
-
 	printList(head1);
+	cout << endl;
+	head1 = rotateLLByK(head1, m);
+	printList(head1);
+
 	return 0;
 }
 
